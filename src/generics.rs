52fd230d3b67b4b1fd5ed142f1af46d65b282c81
varun24143 <varun24143@gmail.com>
2021-08-18 14:56:20 +0530

    generic ways

--- src/generics.rs
+++ src/generics.rs
@@ -4,3 +4,19 @@ Supports generics and even allows to enforce the implementation of certain
 traits. These constraints can come either come as a where clause attached to
 the function definition or with a colon in the generic type of declaration.
 */
+
+pub fn my_generic<T: MyTrait>(t: T) {
+    // code
+}
+
+// this is same as
+
+pub fn my_generic<T>(t: T) where T: MyTrait {
+    // code
+}
+
+// but better use in 2018 and beyond
+
+pub fn my_generic(t: impl MyTrait) {
+    // code
+}
